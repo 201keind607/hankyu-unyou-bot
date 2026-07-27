4110ee1f9f30f223d5814fa1f488f7a6f3e63196
201keind607 <mukchild@gmail.com>
2026-07-28 07:54:45 +0900

    .github/workflows/run.yml

--- bot.py
+++ bot.py
@@ -1,1 +1,30 @@
+name: Hankyu Bot
 
+on:
+  schedule:
+    - cron: "6 15 * * *"
+
+  workflow_dispatch:
+
+jobs:
+  run:
+    runs-on: ubuntu-latest
+
+    steps:
+      - name: Checkout
+        uses: actions/checkout@v4
+
+      - name: Python setup
+        uses: actions/setup-python@v5
+        with:
+          python-version: "3.12"
+
+      - name: Install
+        run: |
+          pip install requests
+
+      - name: Run bot
+        env:
+          DISCORD_WEBHOOK: ${{ secrets.DISCORD_WEBHOOK }}
+        run: |
+          python bot.py
